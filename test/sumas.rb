1058a6aa7342afc4ece06995ec021a585a4e1efe
Pepe Doval <pepellou@gmail.com>
2012-12-11 12:58:22 +0100

    Try better interface - red

--- test/sumas.rb
+++ test/sumas.rb
@@ -10,10 +10,7 @@ end
 describe 'Acceptance Test' do
 
 	it 'gives us all natural sums resulting a number' do
-		sumas = Sumas.new
-		sumas.number = 5
-
-		sumas.getSumas().should eql [ '5+0', '4+1', '3+2' ]
+		5.sums().should eql [ '5+0', '4+1', '3+2' ]
 	end
 
 end
