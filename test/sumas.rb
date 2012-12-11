e4d63ca92fe134fcae445f57c6671ec647ee42b6
Pepe Doval <pepellou@gmail.com>
2012-12-11 12:53:13 +0100

    Acceptance Test - red

--- test/sumas.rb
+++ test/sumas.rb
@@ -1,6 +1,19 @@
 # encoding: utf-8
 require 'rspec'
 
+require_relative '../sumas'
+
 describe 'Sumas' do
+
+end
+
+describe 'Acceptance Test' do
+
+	it 'gives us all natural sums resulting a number' do
+		sumas = Sumas.new
+		sumas.number = 5
+
+		sumas.getSumas().should eql [ '5+0', '4+1', '3+2' ]
+	end
 
 end
