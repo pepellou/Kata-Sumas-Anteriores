764b0b9c02c87d6e80312bb7856dd6301caf3cbd
Pepe Doval <pepellou@gmail.com>
2012-12-11 13:50:50 +0100

    Sums sum number - refactor

--- sumas.rb
+++ sumas.rb
@@ -2,8 +2,8 @@ class Fixnum
 
 	def sums
 		result = [ "#{self}+0" ]
-		(1..(self/2)).each do |i|
-			result.push "#{self - i}+#{i}"
+		(1..(self/2)).each do |n|
+			result.push "#{self - n}+#{n}"
 		end
 		result
 	end
